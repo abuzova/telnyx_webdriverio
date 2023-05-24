29965ed50889c8bd013fbe98e28e1c6b298eb8bd
abuzova <alla.abuzova@gmail.com>
2023-05-24 16:43:04 +0200

    a few changes

--- wdio.conf.js
+++ wdio.conf.js
@@ -53,7 +53,8 @@ export const config = {
     //
     capabilities: [{
         // capabilities for local browser web tests
-        browserName: 'chrome' // or "firefox", "microsoftedge", "safari"
+        //browserName: 'chrome' // or "firefox", "microsoftedge", "safari" 
+        browserName: 'chromium-browser' 
     }, 
     {
         // capabilities for local browser web tests
