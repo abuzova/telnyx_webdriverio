25373df7139bcabe119699752f7dcd6ecb03dea8
abuzova <alla.abuzova@gmail.com>
2023-06-26 16:59:33 +0200

    add BU_017

---
+++ test/helper/constants.js
@@ -0,0 +1,21 @@
+class Constants {     
+
+    EMAIL = 'asd@gmail.com';  
+    PASSWORD = 'wjfji59(djGR43*';
+    FIRST_NAME = 'Anna';
+    LAST_NAME = 'Rodriguez';    
+    HOW_HELP = 'Sales Inquiry';   
+    COUNTRY = 'Spain (+34)';    
+    PHONE_NUMBER = '07575848389';    
+    PRIMARY_INTEREST = 'Conversational Messaging';
+    DESCRIBE_REQUEST = 'Describe your request'
+    HOW_DID_YOU_HEAR = 'Social media';
+    COMPANY_WEB_SITE = 'https://info.biz.ua/'; 
+    COMPANY = 'United Kingdom (+44)';
+    COMPANY_EMAIL = 'comp@gmail.com';
+    PARTNER_TYPE = 'Reseller ';
+    WHY_TELNYX_PARTNER = 'Tell us why you would like to become a Telnyx partner';
+
+}
+
+export default new Constants;
---
+++ test/pageobjects/Blog.page.js
@@ -0,0 +1,35 @@
+import Page from './Page.js';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class BlogPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get goNextPageArrow() {
+        return $('//nav//a/span[contains(text(), "Go to next page")]/parent::a');
+    }
+    
+    get sms_mms_DeliverabilityGuideArticle(){
+        return $('//h3[contains(text(), "SMS/MMS Deliverability Guide 2023")]/following-sibling::div/a');
+    }
+
+    async clickGoNextPageArrow(){
+        await this.goNextPageArrow.click();
+        await expect(browser).toHaveUrlContaining('/resources/page');
+    }
+
+    async clickSmsMms_DeliverabilityGuideArticle(){
+        await this.sms_mms_DeliverabilityGuideArticle.click();
+        await expect(browser).toHaveUrlContaining('/resources/sms-mms-deliverability-guide-2023');
+    }
+
+    /*open () {
+        return super.open('');
+    }*/
+}
+
+export default new BlogPage();
+
+
