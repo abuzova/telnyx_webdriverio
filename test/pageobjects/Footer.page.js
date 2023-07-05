51f15df5edd8db2fd08e5312c719fd65b6398270
abuzova <alla.abuzova@gmail.com>
2023-07-05 15:54:56 +0200

    finished footer_menu_BU_001

--- test/pageobjects/Footer.page.js
+++ test/pageobjects/Footer.page.js
@@ -1,68 +1,98 @@
 import Page from './Page.js';
-//const jsonFileFirst = require('../help/firstFooterMenuItems.json');
-//const jsonFileSecond = require('../help/secondFooterMenuItems.json');
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class FooterPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    /*constructor(iMax, jMax){
-        this.i = 1;
-        this.j = 1;
-        this.iMax = iMax;
-        this.jMax = jMax; 
-        super();   
-    }*/
+   
+    get homeFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Data and Privacy")]');
+    }  
 
-    get firstFooterMenuItem() {
-        //return $('footer div:nth-child(1) div:nth-child(2) ul li:nth-child(1) a');
-        //return $('footer div:nth-child(1) div:nth-child(2) ul');
-
-        const text = $$(function() { // Arrow function is not allowed here.
-            // this is Window https://developer.mozilla.org/en-US/docs/Web/API/Window
-            // TypeScript users may do something like this
-            // return (this as Window).document.querySelectorAll('#menu')
-            return this.document.querySelectorAll('footer div:nth-child(1) div:nth-child(2) ul'); // Element[]
-        })[0]       
+    get reportAbuseFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Report Abuse")]');
     }
 
-    /*get secondFooterMenuItem() {
-        return $('footer div:nth-child(1) div:nth-child(3) ul ');
-    }*/
-
-    /*get btnSubmit () {
-        return $('button[type="submit"]');
-    }*/
-
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    /*async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
-    }*/
-
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    open () {
-        return super.open('');
+    get privacyPolicyFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Privacy Policy")]');
     }
 
-    /*async haveAttrHrefFirstFooterMenu(i){        
-        await expect(this.firstFooterMenuItem().$('li:nth-child('+ i +') a')).toHaveAttribute('href', jsonFile.firstFooterMenuItem[this.i]);
-    }*/
+    get coolkiePolicyFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Cookie Policy")]');
+    }
 
-    /*async haveAttrHrefSecondFooterMenu(j){        
-        await expect(this.secondFooterMenuItem().$('li:nth-child('+ j +') a')).toHaveAttribute('href', jsonFile.secondFooterMenuItem[this.j]);
-    }*/
+    get lawEnforcementFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Law Enforcement")]');
+    }
+
+    get acceptableUseFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Acceptable Use")]');
+    }
+
+    get releaseNotesFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Release Notes")]');
+    }
+
+    get careersFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Careers")]');
+    }
+
+    get websiteTermsAndConditionsFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Website Terms and Conditions")]');
+    }
+
+    get termsAndConditionsOfServiceFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "Terms and conditions of service")]');
+    }
+
+    get betterTwiltioFooterMenuItem(){
+        return $('//footer//ul/li/a[contains(text(), "The Better Twilio Alternative")]');
+    }
 
 
+    async haveAttrHomeFooterMenuItem(){
+        await expect(this.homeFooterMenuItem).toHaveAttribute('href', '/data-privacy'); 
+    }
+
+
+    async haveAttrReportAbuseFooterMenuItem(){
+        await expect(this.reportAbuseFooterMenuItem).toHaveAttribute('href', '/report-abuse');
+    }
+
+    async haveAttrPrivacyPolicyFooterMenuItem(){
+        await expect(this.privacyPolicyFooterMenuItem).toHaveAttribute('href', '/privacy-policy');
+    }
+    
+
+    async haveAttrCoolkiePolicyFooterMenuItem(){
+        await expect(this.coolkiePolicyFooterMenuItem).toHaveAttribute('href', '/cookie-policy');
+    }
+
+    async haveAttrLawEnforcementFooterMenuItem(){
+        await expect(this.lawEnforcementFooterMenuItem).toHaveAttribute('href', '/law-enforcement-request');
+    }
+
+    async haveAttrAcceptableUseFooterMenuItem(){
+        await expect(this.acceptableUseFooterMenuItem).toHaveAttribute('href', '/acceptable-use-policy');
+    }
+
+    async haveAttrReleaseNotesFooterMenuItem(){
+        await expect(this.releaseNotesFooterMenuItem).toHaveAttribute('href', '/release-notes');
+    }
+
+    async haveAttrCareersFooterMenuItem(){
+        await expect(this.careersFooterMenuItem).toHaveAttribute('href', '/careers');
+    }
+
+    async haveAttrWebsiteTermsAndConditionsFooterMenuItem(){
+        await expect(this.websiteTermsAndConditionsFooterMenuItem).toHaveAttribute('href', '/terms-and-conditions');
+    }
+
+    async haveAttrTermsAndConditionsOfServiceFooterMenuItem(){
+        await expect(this.termsAndConditionsOfServiceFooterMenuItem).toHaveAttribute('href', '/terms-and-conditions-of-service');
+    }
+
+    async haveAttrBetterTwiltioFooterMenuItem(){
+        await expect(this.betterTwiltioFooterMenuItem).toHaveAttribute('href', '/the-better-twilio-alternative');
+    }
+  
 }
 
 export default new FooterPage();
